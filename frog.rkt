49312b4f9b6e80e4a1343297af117bb176bd55a6
Alexis King <lexi.lambda@gmail.com>
2017-06-28 15:46:17 -0700

    Migrate .frogrc to frog.rkt

---
+++ frog.rkt
@@ -0,0 +1,40 @@
+#lang frog/config
+
+;; Called early when Frog launches. Use this to set parameters defined
+;; in frog/params.
+(define/contract (init)
+  (-> any)
+  (current-scheme/host "http://lexi-lambda.github.io")
+  (current-uri-prefix #f)
+  (current-title "Alexis King's Blog")
+  (current-author "Alexis King")
+  (current-editor "$EDITOR")
+  (current-editor-command "{editor} {filename}")
+  (current-show-tag-counts? #t)
+  (current-permalink "/blog/{year}/{month}/{day}/{title}/index.html")
+  (current-index-full? #f)
+  (current-feed-full? #t)
+  (current-max-feed-items 20)
+  (current-decorate-feed-uris? #t)
+  (current-feed-image-bugs? #t)
+  (current-posts-per-page 10)
+  (current-index-newest-first? #t)
+  (current-posts-index-uri "/index.html")
+  (current-source-dir "_src")
+  (current-output-dir "out"))
+
+;; Called once per post and non-post page, on the contents.
+(define/contract (enhance-body xs)
+  (-> (listof xexpr/c) (listof xexpr/c))
+  ;; Here we pass the xexprs through a series of functions.
+  (~> xs
+      (syntax-highlight #:python-executable "python"
+                        #:line-numbers? #f
+                        #:css-class "source")
+      (auto-embed-tweets #:parents? #t)
+      (add-racket-doc-links #:code? #t #:prose? #t)))
+
+;; Called from `raco frog --clean`.
+(define/contract (clean)
+  (-> any)
+  (void))
