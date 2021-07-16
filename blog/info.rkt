639e3f98f0b11649ff9eba25ebb43cff731f476b
Alexis King <lexi.lambda@gmail.com>
2021-07-16 02:03:32 -0500

    Add an info.rkt file for blog/
    Also, remove the half-baked attempt at a Racket-native Haskell syntax
    highlighter.

---
+++ blog/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+
+(define collection "blog")
+
+(define deps
+  '("base"
+    "functional-lib"
+    "megaparsack-lib"
+    "racket-index"
+    "scribble-lib"
+    "threading-lib"))
+(define build-deps
+  '("at-exp-lib"))
